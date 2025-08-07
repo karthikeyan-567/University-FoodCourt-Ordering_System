6bdec3ce8086b9a91e9e8a82bd25a5a86a60aa0d
Melwin31 <melwin272005@gmail.com>
2025-08-07 07:29:00 +0530

    update

--- client/src/Home/home.jsx
+++ client/src/Home/home.jsx
@@ -1,14 +1,28 @@
-
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Categories from './components/Categories';
-
+import Menucard from '../components/menucard';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Categories from '../components/Categories';
+import './home.css';
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
       <Categories />
+      <div style={{ display:'flex' , flexWrap: 'wrap',  justifyContent: 'center',gap: '20px', padding: '10px' }}>
+       <Menucard name="Dosai" price="Rs250" img  ="https://i.pinimg.com/1200x/ed/39/5a/ed395a2799679158edc7f2cc3520bb69.jpg" /> 
+        <Menucard name="Chicken Biryani" price="Rs250"  img="https://i.pinimg.com/1200x/92/48/d2/9248d2de57951839856b5166785d8453.jpg" /> 
+         <Menucard name="Paraota" price="Rs250"  img="https://i.pinimg.com/736x/72/ef/4b/72ef4b8ca201637e3aa8979550880f60.jpg" /> 
+          <Menucard name="Chicken Gravy" price="Rs250"  img="https://i.pinimg.com/736x/01/de/4b/01de4b545bb768d801350894727e6862.jpg" /> 
+           <Menucard name="Chicken Biryani" price="Rs250"  img="https://i.pinimg.com/1200x/5d/fa/3d/5dfa3d14c019dbcf727504a732151dda.jpg" /> 
+        <Menucard name="Chicken Biryani" price="Rs250" img="https://i.pinimg.com/736x/80/30/72/8030727314b1b950376d0dc83e249add.jpg" /> 
+         <Menucard name="panneer" price="Rs250" img="https://i.pinimg.com/736x/a7/9a/5b/a79a5b8ade6634bb288e8fa0a67f9d32.jpg" />
+            <Menucard name="Meals" price="Rs250"  img="https://i.pinimg.com/1200x/f7/41/5f/f7415f93ecdccc842224b4790f541112.jpg" /> 
+             <Menucard name="Chicken Biryani" price="Rs250"  img="https://i.pinimg.com/1200x/5d/fa/3d/5dfa3d14c019dbcf727504a732151dda.jpg" /> 
+        <Menucard name="Chicken Biryani" price="Rs250" img="https://i.pinimg.com/736x/80/30/72/8030727314b1b950376d0dc83e249add.jpg" /> 
+         <Menucard name="panneer" price="Rs250" img="https://i.pinimg.com/736x/a7/9a/5b/a79a5b8ade6634bb288e8fa0a67f9d32.jpg" />
+            <Menucard name="Meals" price="Rs250"  img="https://i.pinimg.com/1200x/f7/41/5f/f7415f93ecdccc842224b4790f541112.jpg" /> 
+      </div>
     </>
   );
 }
--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,40 +1,10 @@
 import './Categories.css';
+import { Router } from 'react-router-dom';
 import { useState } from 'react';
-const categories = [
-  { name: 'chicken Briyani', price: 'Rs250', img: '/images/briyani.png' },
-  { name: 'Lollipop Chicken', price: 'Rs250', img: '/images/lloilop.png' },
-  { name: 'Shawarma', price: 'Rs250', img: '/images/shawarma.png' },
-  { name: 'mexicon fried Burgers', price: 'Rs250', img: '/images/salad.png' },
-    { name: 'chicken Briyani', price: 'Rs250', img: '/images/briyani.png' },
-  { name: 'Lollipop Chicken', price: 'Rs250', img: '/images/lloilop.png' },
-  { name: 'Shawarma', price: 'Rs250', img: '/images/shawarma.png' },
-  { name: 'mexicon fried Burgers', price: 'Rs250', img: '/images/salad.png' },
-      { name: 'chicken Briyani', price: 'Rs250', img: '/images/briyani.png' },
-  { name: 'Lollipop Chicken', price: 'Rs250', img: '/images/lloilop.png' },
-  { name: 'Shawarma', price: 'Rs250', img: '/images/shawarma.png' },
-  { name: 'mexicon fried Burgers', price: 'Rs250', img: '/images/salad.png' }
-];
+
 
 const Categories = () => {
-  const [counts, setCounts] = useState(Array(categories.length).fill(0));
-
-  const add = (index) => {
-    setCounts((prevCounts) => {
-      const updated = [...prevCounts];
-      updated[index] += 1;
-      return updated;
-    });
-  };
-
-  const subtract = (index) => {
-    setCounts((prevCounts) => {
-      const updated = [...prevCounts];
-      if (updated[index] > 0) {
-        updated[index] -= 1;
-      }
-      return updated;
-    });
-  };
+      
   return (
     <section className="categories-section">
       <h2 className="categories-heading">Top Categories</h2>
@@ -49,21 +19,7 @@ const Categories = () => {
       </div>
       <h3 className="menu-heading">Today's Menu</h3>
 
-      <div className="menu-grid">
-        {categories.map((item, idx) => (
-          <div key={idx} className="menu-card">
-            <img src={item.img} alt={item.name} />
-            <h3>{item.name}</h3>
-            <p className="price">{item.price}</p>
-            <div className="quantity-controls">
-              <button className="subtract-btn" onClick={() => subtract(idx)}>-</button>
-              <span className="count">{counts[idx]}</span>
-              <button className="add-btn" onClick={() => add(idx)}>+</button>
-            </div>
-             <button className="order-btn">Order Now</button>
-          </div>
-        ))}
-      </div>
+   
     </section>
   );
 };
